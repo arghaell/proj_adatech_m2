e17009f4f64cc9113b8cbe247dd7693ec859f7c6
heldersantos764 <heldersantos764@gmail.com>
2023-11-22 22:21:34 -0300

    Construção do botão Ver Todos e Finalização do Projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,9 @@ const formSearch = document.querySelector("#form-search");
 let idContactEdit;
 let isRegister = true;
 const actionTypes = {
-  register: "register",
-  edit: "edit",
-  destroy: "destroy",
+    register: "register",
+    edit: "edit",
+    destroy: "destroy",
 };
 
 /**
@@ -16,16 +16,16 @@ const actionTypes = {
  * @returns
  */
 const getDataForm = () => {
-  return {
-    name: document.querySelector('#form input[name="name"]').value,
-    phone: document.querySelector('#form input[name="phone"]').value,
-    email: document.querySelector('#form input[name="email"]').value,
-    street: document.querySelector('#form input[name="street"]').value,
-    number: document.querySelector('#form input[name="number"]').value,
-    district: document.querySelector('#form input[name="district"]').value,
-    city: document.querySelector('#form input[name="city"]').value,
-    state: document.querySelector('#form input[name="state"]').value,
-  };
+    return {
+        name: document.querySelector('#form input[name="name"]').value,
+        phone: document.querySelector('#form input[name="phone"]').value,
+        email: document.querySelector('#form input[name="email"]').value,
+        street: document.querySelector('#form input[name="street"]').value,
+        number: document.querySelector('#form input[name="number"]').value,
+        district: document.querySelector('#form input[name="district"]').value,
+        city: document.querySelector('#form input[name="city"]').value,
+        state: document.querySelector('#form input[name="state"]').value,
+    };
 };
 
 /**
@@ -33,92 +33,92 @@ const getDataForm = () => {
  * @param {*} register
  */
 const openRegisterModal = (register) => {
-  isRegister = register;
+    isRegister = register;
 
-  if (isRegister) {
-    document.querySelector("#title-modal-register").innerHTML = "Novo Contato";
-    document.querySelector("#btn-modal-register").innerHTML = "Cadastrar";
-  } else {
-    document.querySelector("#title-modal-register").innerHTML =
-      "Editar Contato";
-    document.querySelector("#btn-modal-register").innerHTML = "Salvar";
-  }
+    if (isRegister) {
+        document.querySelector("#title-modal-register").innerHTML = "Novo Contato";
+        document.querySelector("#btn-modal-register").innerHTML = "Cadastrar";
+    } else {
+        document.querySelector("#title-modal-register").innerHTML =
+            "Editar Contato";
+        document.querySelector("#btn-modal-register").innerHTML = "Salvar";
+    }
 
-  var modalContact = new bootstrap.Modal(document.querySelector("#contact"));
-  modalContact.show();
+    var modalContact = new bootstrap.Modal(document.querySelector("#contact"));
+    modalContact.show();
 };
 
 /**
  * Insere um contato
  */
 const saveContact = () => {
-  const contact = getDataForm();
+    const contact = getDataForm();
 
-  if (isRegister) {
-    insert(contact);
-    alert("Contato cadastrado com sucesso");
-  } else {
-    update(idContactEdit, contact);
-    alert("Contato alterado com sucesso");
-  }
+    if (isRegister) {
+        insert(contact);
+        alert("Contato cadastrado com sucesso");
+    } else {
+        update(idContactEdit, contact);
+        alert("Contato alterado com sucesso");
+    }
 
-  renderTable(findAll());
-  bootstrap.Modal.getInstance(document.querySelector("#contact")).hide();
+    renderTable(findAll());
+    bootstrap.Modal.getInstance(document.querySelector("#contact")).hide();
 };
 
 /**
  * Adiciona os eventos nos botões excluir da tabela
  */
 const addEventToButtonsDestroy = () => {
-  const buttons = document.getElementsByClassName("btn-destroy");
+    const buttons = document.getElementsByClassName("btn-destroy");
 
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-      let contactId = this.getAttribute("id-contact");
-      alert(`Contato excluído com sucesso.`);
-      destroy(contactId);
-      renderTable(findAll());
-    });
-  }
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            let contactId = this.getAttribute("id-contact");
+            alert(`Contato excluído com sucesso.`);
+            destroy(contactId);
+            renderTable(findAll());
+        });
+    }
 };
 
 /**
  * Adiciona os eventos nos botões editar da tabela
  */
 const addEventToButtonsEdit = () => {
-  const buttons = document.querySelectorAll(".btn-edit");
+    const buttons = document.querySelectorAll(".btn-edit");
 
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-      idContactEdit = this.getAttribute("id-contact");
-      let contact = findById(idContactEdit)[0];
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            idContactEdit = this.getAttribute("id-contact");
+            let contact = findById(idContactEdit)[0];
 
-      for (const key in contact) {
-        if (key != "id") {
-          document.querySelector(`#form input[name="${key}"]`).value =
-            contact[key];
-        }
-      }
-      openRegisterModal(false);
-    });
-  }
+            for (const key in contact) {
+                if (key != "id") {
+                    document.querySelector(`#form input[name="${key}"]`).value =
+                        contact[key];
+                }
+            }
+            openRegisterModal(false);
+        });
+    }
 };
 
 /**
  * Monta a tabela depois que alguma alteração ocorrer na lista de contatos
  */
 const renderTable = (contacts) => {
-  let html = "";
+    let html = "";
 
-  if (contacts.length === 0) {
-    html = `
+    if (contacts.length === 0) {
+        html = `
         <tr>
             <td class="text-center " colspan="6">Nenhum Contato adicionado</td>
         </tr>
         `;
-  } else {
-    contacts.forEach((contact) => {
-      html += `
+    } else {
+        contacts.forEach((contact) => {
+            html += `
                 <tr>
                     <td>${contact.id}</td>
                     <td>${contact.name}</td>
@@ -131,46 +131,50 @@ const renderTable = (contacts) => {
                     </td>
                 </tr>
             `;
-    });
-  }
+        });
+    }
 
-  table.innerHTML = html;
-  addEventToButtonsDestroy();
-  addEventToButtonsEdit();
+    table.innerHTML = html;
+    addEventToButtonsDestroy();
+    addEventToButtonsEdit();
 };
 
 /**
  * Adiciona o evento de submit no formulario de cadastrar e editar contato
  */
 formContact.addEventListener("submit", (event) => {
-  event.preventDefault();
-  formSearch.reset();
-  saveContact();
+    event.preventDefault();
+    formSearch.reset();
+    saveContact();
 });
 
 formSearch.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let id = document.querySelector("#search").value;
-  let result = findById(id);
+    event.preventDefault();
+    let id = document.querySelector("#search").value;
+    let result = findById(id);
 
-  bootstrap.Modal.getInstance(document.querySelector("#find-contact")).hide();
+    bootstrap.Modal.getInstance(document.querySelector("#find-contact")).hide();
 
-  if (result.length > 0) {
-    renderTable(result);
-  } else {
-    alert("Contato não encontrado");
-    renderTable(findAll());
-  }
+    if (result.length > 0) {
+        renderTable(result);
+    } else {
+        alert("Contato não encontrado");
+        renderTable(findAll());
+    }
 });
+
+document.querySelector("#see-all").addEventListener('click', e => {
+    renderTable(findAll());
+})
 
 /**
  * Adiciona o evento para abrir modal no modo cadastrar contato
  */
 document
-  .querySelector(".btn-open-register-modal")
-  .addEventListener("click", (e) => {
-    formContact.reset();
-    openRegisterModal(true);
-  });
+    .querySelector(".btn-open-register-modal")
+    .addEventListener("click", (e) => {
+        formContact.reset();
+        openRegisterModal(true);
+    });
 
 renderTable(findAll());
